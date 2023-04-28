2e375b624d02368553454752aefe623607af56a8
Eisuke Esaki <eisukeesaki@gmail.com>
2023-04-28 13:34:21 -0700

    implemented endpoint /:package/:version

--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,12 @@ import client from 'axios';
 
 const app = express();
 
+/**
+  * routes
+  */
 app.get('/:package', (req: Request, res: Response) => {
   const pkg = req.params.package;
+
   client.get(`https://registry.npmjs.com/${pkg}`)
     .then(function(response) {
       const data = response.data;
@@ -21,7 +25,20 @@ app.get('/:package', (req: Request, res: Response) => {
       res.sendStatus(500);
     });
 });
+app.get('/:package/:version', (req: Request, res: Response) => {
+  const pkg = req.params.package;
+  const ver = req.params.version;
 
+  client.get(`https://registry.npmjs.com/${pkg}/${ver}`)
+    .then(function(response) {
+      const data = response.data;
+      res.status(200).json(data);
+    })
+    .catch(function(error) {
+      console.log(error);
+      res.sendStatus(500);
+    });
+});
 app.get('/search', (req: Request, res: Response) => {
   client.get('https://registry.npmjs.com/-/v1/search', {
     params: {
