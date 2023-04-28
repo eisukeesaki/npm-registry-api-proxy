56629b8e18ec100616edd38861418646cb2050e8
Eisuke Esaki <eisukeesaki@gmail.com>
2023-04-28 12:53:38 -0700

    implemented endpoint /search

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,24 @@
 ##############################################################################*/
 
 import express, { Request, Response } from 'express';
+import client from 'axios';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.end('you got me');
+app.get('/search', (req: Request, res: Response) => {
+  client.get('https://registry.npmjs.com/-/v1/search', {
+    params: {
+      text: req.query.text,
+    }
+  })
+    .then(function(response) {
+      const data = response.data;
+      res.status(200).json(data);
+    })
+    .catch(function(error) {
+      console.log(error);
+      res.sendStatus(500);
+    });
 });
 
 export default app;
