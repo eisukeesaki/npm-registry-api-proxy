084336a01fe7c481afb9b2f76f5c9d2a2955bf6e
Eisuke Esaki <eisukeesaki@gmail.com>
2023-05-07 22:01:57 -0700

    added entities and relationships: users, usages

--- appdata/sql/migration.sql
+++ appdata/sql/migration.sql
@@ -14,24 +14,66 @@ CREATE OR REPLACE FUNCTION update_modified_column()
     END;
     $$ language 'plpgsql';
 
--- REQUESTS
-CREATE TABLE requests (
-    id
-        UUID
+-- USERS
+CREATE TABLE users (
+    id UUID
         PRIMARY KEY
-        DEFAULT gen_random_uuid()
-        NOT NULL,
-    headers
-        jsonb,
-    time
-        VARCHAR(500)
-        NOT NULL,
-    created
-        TIMESTAMP
+        DEFAULT gen_random_uuid(),
+    username VARCHAR(80)
+        NOT NULL
+        UNIQUE,
+    api_key VARCHAR(1000)
+        NOT NULL
+        UNIQUE,
+    created TIMESTAMP
         NOT NULL
         DEFAULT (CURRENT_TIMESTAMP), 
-    modified
-        TIMESTAMP
+    modified TIMESTAMP
+);
+CREATE TRIGGER update_users_modtime
+    BEFORE UPDATE
+    ON users
+    FOR EACH ROW
+        EXECUTE PROCEDURE update_modified_column();
+
+-- USAGE
+CREATE TABLE usages (
+    id UUID
+        PRIMARY KEY
+        DEFAULT gen_random_uuid(),
+    date_ VARCHAR(40)
+        NOT NULL,
+    count_ integer
+        NOT NULL,
+    user_id UUID
+        REFERENCES users
+        NOT NULL,
+    created TIMESTAMP
+        NOT NULL
+        DEFAULT (CURRENT_TIMESTAMP), 
+    modified TIMESTAMP
+);
+CREATE TRIGGER update_usages_modtime
+    BEFORE UPDATE
+    ON usages
+    FOR EACH ROW
+        EXECUTE PROCEDURE update_modified_column();
+ 
+-- REQUESTS
+CREATE TABLE requests (
+    id UUID
+        PRIMARY KEY
+        DEFAULT gen_random_uuid(),
+    time VARCHAR(500)
+        NOT NULL,
+    headers jsonb,
+    user_id UUID
+        REFERENCES users
+        NOT NULL,
+    created TIMESTAMP
+        NOT NULL
+        DEFAULT (CURRENT_TIMESTAMP), 
+    modified TIMESTAMP
 );
 CREATE TRIGGER update_requests_modtime
     BEFORE UPDATE
@@ -41,31 +83,29 @@ CREATE TRIGGER update_requests_modtime
 
 -- RESPONSES
 CREATE TABLE responses (
-    id
-        UUID
+    id UUID
         PRIMARY KEY
-        DEFAULT gen_random_uuid()
+        DEFAULT gen_random_uuid(),
+    time VARCHAR(500)
         NOT NULL,
-    request_id
-        UUID
+    headers jsonb,
+    body jsonb,
+    request_id UUID
         REFERENCES requests
         NOT NULL,
-    headers
-        jsonb,
-    time
-        VARCHAR(500)
-        NOT NULL,
-   body
-        jsonb,
-   created
-        TIMESTAMP
+    created TIMESTAMP
         NOT NULL
         DEFAULT (CURRENT_TIMESTAMP), 
-    modified
-        TIMESTAMP
+    modified TIMESTAMP
 );
 CREATE TRIGGER update_responses_modtime
     BEFORE UPDATE
     ON responses
     FOR EACH ROW
         EXECUTE PROCEDURE update_modified_column();
+
+ALTER TABLE requests
+    ADD COLUMN
+        response_id UUID
+            REFERENCES responses;
+
