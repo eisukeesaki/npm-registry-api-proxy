e2689c28a2b455a39eac85cc1a4915fa8bacb05b
Eisuke Esaki <eisukeesaki@gmail.com>
2023-05-09 23:59:38 -0700

    formatting fix

--- src/routes/registeration.ts
+++ src/routes/registeration.ts
@@ -31,22 +31,23 @@ async function createUser(username: string, req: Request): Promise<User> {
   return user.rows[0];
 }
 
-router.post('/registration', async (req: Request, res: Response): Promise<void> => {
-  console.trace('req.body: %o', req.body);
-  try {
-    // @todo validate POST body
-    const username = req.body.username;
+router.post('/registration',
+  async (req: Request, res: Response): Promise<void> => {
+    console.trace('req.body: %o', req.body);
+    try {
+      // @todo validate POST body
+      const username = req.body.username;
 
-    if (username) {
-      const user = await createUser(username, req);
+      if (username) {
+        const user = await createUser(username, req);
 
-      res.status(201).json(user);
-    } else
-      res.status(400).send('invalid argument');
-  } catch (err) {
-    console.trace('failed to create new user record in db', err);
-    res.sendStatus(500);
-  }
-});
+        res.status(201).json(user);
+      } else
+        res.status(400).send('invalid argument');
+    } catch (err) {
+      console.trace('failed to create new user record in db', err);
+      res.sendStatus(500);
+    }
+  });
 
 export default router;
